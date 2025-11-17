950115d844d4b3a7f4b7172f273e0ad787badb5a
Tien <phamviettien182173@gmail.com>
2025-11-18 02:38:07 +0700

    Add AimsProject folder

---
+++ AimsProject/src/com/hust/kstn/Aims.java
@@ -0,0 +1,34 @@
+package com.hust.kstn;
+
+import com.hust.kstn.models.Cart;
+import com.hust.kstn.models.DigitalVideoDisc;
+
+public class Aims {
+    public static void main(String[] args) {
+        Cart cart = new Cart();
+
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("Your Name", "Romance/Fantasy","Makoto Shinkai", 107, 29.99);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("One Punch Man", "Action/Sci-Fi","Shingo Natsume", 120, 35.50);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Kids/Comedy", "Doraemon", 15.00); 
+            DigitalVideoDisc dvd4 = new DigitalVideoDisc("Armor Hero");
+
+        cart.addDVD(dvd1); 
+        cart.addDVD(dvd2); 
+        cart.addDVD(dvd3);
+        
+        cart.print();
+        
+        String removedTitle = dvd2.getTitle();
+        cart.removeDVD(dvd2); 
+        
+        System.out.println("\n--- Cart after removing " + removedTitle + " ---");
+        cart.print();
+        cart.print();
+        
+        System.out.println("\n--- Testing edge cases ---");
+        cart.removeDVD(dvd4); 
+        
+        cart.addDVD(dvd4);
+        cart.addDVD(new DigitalVideoDisc("Test Full"));
+    }
+}
---
+++ AimsProject/src/com/hust/kstn/models/Cart.java
@@ -0,0 +1,61 @@
+package com.hust.kstn.models;
+public class Cart {
+    private static final int MAX_NUMBER_ORDERED = 20; 
+    private DigitalVideoDisc[] itemsInCart = new DigitalVideoDisc[MAX_NUMBER_ORDERED];
+    private int qtyOrdered = 0;
+
+    public void addDVD(DigitalVideoDisc disc) {
+        if (qtyOrdered < MAX_NUMBER_ORDERED) {
+            itemsInCart[qtyOrdered] = disc;
+            qtyOrdered++;
+            System.out.println("The disc has been added sucessfully");
+        } else {
+            System.out.println("The cart is almost full. Cannot add more DVD.");
+        }
+    }
+
+    public void removeDVD(DigitalVideoDisc disc) {
+        if (qtyOrdered == 0) {
+            System.out.println("The cart is empty. Cannot remove DVD.");
+            return;
+        }
+
+        boolean found = false;
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsInCart[i] == disc) { 
+                found = true;
+                for (int j = i; j < qtyOrdered - 1; j++) {
+                    itemsInCart[j] = itemsInCart[j + 1];
+                }
+                itemsInCart[qtyOrdered - 1] = null;
+                qtyOrdered--;
+                System.out.println("The disc has been removed sucessfully");
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("The disc does not exist in the cart.");
+        }
+    }
+
+    public double calculateTotalCost() {
+        double totalCost = 0.0;
+        for (int i = 0; i < qtyOrdered; i++) { 
+            totalCost += itemsInCart[i].getCost();
+        }
+        return totalCost;
+    }
+
+    public void print() {
+        System.out.println("***********************CART***********************");
+        System.out.println("Order:");
+        for (int i = 0; i < qtyOrdered; i++) {
+            DigitalVideoDisc item = itemsInCart[i];
+            System.out.printf("%d. DVD - [%s] - [%s] - [%s] - %d minutes: %.2f $\n",
+                i + 1, item.getTitle(), item.getCategory(), item.getDirector(), item.getLength(), item.getCost());
+        }
+        System.out.println("Total Cost: " + calculateTotalCost());
+        System.out.println("**************************************************");
+    }
+}
